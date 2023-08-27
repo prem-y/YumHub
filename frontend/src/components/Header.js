a2cd13bf10c525f2b024e6c0ab487a014dd0aaab
Prem-rao <raopremwaghmare@gmail.com>
2023-08-27 10:16:23 +0530

    added header to homepage

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import {Link} from 'react-router-dom';
+
+const Header = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+    const logo ={
+        fontFamily:"'Concert One', cursive",
+        fontSize: "2em",
+        position:"relative",
+        top:"0.2em",
+        left:"0.3em"
+    }
+  return (
+    <>
+    <nav className='navbar-expand-lg bg-danger p-1'>
+        <ul className="d-flex justify-content-between list-unstyled ">
+            <li>
+            <Link to={'/home'} className="text-decoration-none text-white" style={logo}>YumHub</Link>
+            </li>
+            <li>
+                <form className="mx-2 d-flex my-1">
+                        <li>
+                         <input type="search" name="" id="" placeholder="Search for recipes.." className="form-control mt-2"/>
+                        </li>
+                        <li>
+                            <button className="mt-2 mx-2 form-control bg-outline-dark">Search</button>
+                        </li>
+
+                </form>
+            </li>
+            <li>
+            <div className="mt-3 fs-5">
+                <ul className=" d-flex flex-row list-unstyled ">
+                    <li className="">
+                    <Link to={'veg'} className="text-decoration-none m-3 text-white">Veg</Link>
+                    </li>
+                    <li>
+                    <Link to={'/non-veg'} className="text-decoration-none m-3 text-white">Non-Veg</Link>
+                    </li>
+                    <li>
+                    <Link to={'/myprofile'} className="text-decoration-none m-3 text-white">My Profile</Link> 
+                    </li>
+                </ul>
+            </div>
+            </li>
+        </ul>
+    </nav>
+    </>
+  );
+};
+
+export default Header;
