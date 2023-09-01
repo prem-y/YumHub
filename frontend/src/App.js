a024e465511b425030ff80b5481311e4044429a2
Santosh091002 <naiks6230@gmail.com>
2023-09-01 17:40:37 +0530

    created footer file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Landingpage from "./pages/Landingpage"
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
 import Home from "./pages/Home"
+
 function App() {
   return (
     <>
