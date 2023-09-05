82992e63d0185b662872c492e16b0962b64f5cc6
Prem-rao <raopremwaghmare@gmail.com>
2023-09-05 10:14:19 +0530

    commit

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,75 +1,92 @@
-import React, {useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
-
 import { useParams } from 'react-router-dom';
+import Footer from '../components/Footer';
 
-import Footer from "../components/Footer";
 const Home = () => {
-  const [recipes, setRecipes] = useState([]);
-  const [searchQuery, setSearchQuesry] = useState([]);
-  useEffect(()=>{
+  const [recipes, setRecipes] = useState("");
+  const [searchQuery, setSearchQuery] = useState('');
+
+  useEffect(() => {
     fetchRecipes();
-  })
+  }, []); // Empty dependency array to run fetchRecipes only once on mount
 
   const params = useParams();
   let userId = params.user;
 
-  const fetchRecipes =async () =>{
-    axios.get(`http://localhost:4000/api/recipes?title=${searchQuery}`)
-    .then((response)=>{
-      setRecipes(response.data);
-    })
-    .catch((error)=>{
-      console.error(error);
-    });
+  const fetchRecipes = () => {
+    axios
+      .get(`http://localhost:4000/api/recipes?title=${searchQuery}`)
+      .then((response) => {
+        setRecipes(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
+
+  const searchHandler = (e) => {
+    e.preventDefault();
+    fetchRecipes(); // Trigger a new fetch when the search form is submitted
+  };
+
   return (
     <>
-      <Header userId={userId}/>
+      <Header userId={userId} />
       <div className="container mt-5">
-      <form className="d-flex">
-      <input type="search" name="" id="" placeholder="Search for recipes.." className="form-control "
-      onChange={(event)=>{
-        setSearchQuesry(event.target.value);
-      }}/>
-      </form>
-      <br />
-      {
-
-        !recipes?(<p>No recipe found</p>):(
+        <form className="d-flex" onSubmit={(e) => searchHandler(e)}>
+          <input
+            type="search"
+            name=""
+            id=""
+            placeholder="Search for recipes.."
+            className="form-control"
+            value={searchQuery} // Add value attribute to input field
+            onChange={(e) => setSearchQuery(e.target.value)} // Update searchQuery state
+          />
+          <button type="submit">Search</button> 
+        </form>
+        <br />
+        {!recipes?(
           <>
-        {recipes && (
-          <div className="row">
-            {recipes.map((recipe) => (
-              <div key={recipe.id} className="col-md-4">
-                <div className="card">
-                <img src={recipe.image} alt="image is expired!" class="card-img-top"/>
-                  <div className="card-body">
-                    <h3 className="card-title">{recipe.title}</h3>
-                    <ul className='d-flex flex-row list-unstyled justify-content-between'>
-                      <li>
-                      <span><img src="./assets/clock.png" alt="" height={'50px'}/></span>
-                      <span className='h4'>{recipe.cooking_time}min</span>
-                      </li>
-                      <li><Link to={{pathname:`/details/${userId}/${recipe._id}`}} className='btn bg-dark text-white'>More info</Link></li>
-                    </ul>
-                  
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        )}
+              <p>No recipes found!</p>
           </>
-        )
-      }
+        ) : (
+          <>
+          <div className="row">
+                {recipes.map((recipe) => (
+                  <div key={recipe.id} className="col-md-4">
+                    <div className="card">
+                      <img src={recipe.image} alt="image is expired!" className="card-img-top" />
+                      <div className="card-body">
+                        <h3 className="card-title">{recipe.title}</h3>
+                        <ul className="d-flex flex-row list-unstyled justify-content-between">
+                          <li>
+                            <span>
+                              <img src="./assets/clock.png" alt="" height={'50px'} />
+                            </span>
+                            <span className="h4">{recipe.cooking_time}min</span>
+                          </li>
+                          <li>
+                            <Link to={{ pathname: `/details/${userId}/${recipe._id}` }} className="btn bg-dark text-white">
+                              More info
+                            </Link>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+           
+          </>
+        )}
       </div>
-      <Footer/>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Home
-
+export default Home;
--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import Footer from '../components/Footer';
 import UserRecipes from '../components/userRecipes';
+import {Link} from 'react-router-dom';
 import '../CSS/Profile.css'; // Import the CSS file
 
 const Profile = () => {
@@ -17,6 +18,7 @@ const Profile = () => {
     nutritional_info: '',
     cuisine_type: '',
     image: '',
+    username: `${userId}`,
   });
 
   const handleSubmit = async (event) => {
@@ -37,6 +39,7 @@ const Profile = () => {
           nutritional_info: '',
           cuisine_type: '',
           image: '',
+          username: `${userId}`,
         });
       } else {
         alert('Recipe creation failed. Please try again.');
@@ -54,9 +57,38 @@ const Profile = () => {
       [name]: value,
     });
   };
+  const logo ={
+    fontFamily:"'Concert One', cursive",
+    fontSize: "2em",
+    position:"relative",
+    top:"0.2em",
+    left:"0.3em"
+  }
 
   return (
     <>
+    <nav className='navbar-expand-lg bg-danger p-1'>
+        <ul className="d-flex justify-content-between list-unstyled ">
+            <li>
+            <Link to={'/home'} className="text-decoration-none text-white" style={logo}>YumHub</Link>
+            </li>
+            <li>
+            <div className="mt-3 fs-5">
+                <ul className=" d-flex flex-row list-unstyled ">
+                    <li className="">
+                    <Link to={'veg'} className="text-decoration-none m-3 text-white">Veg</Link>
+                    </li>
+                    <li>
+                    <Link to={`/home/${userId}`} className="text-decoration-none m-3 text-white">Home</Link>
+                    </li>
+                    <li>
+                    <Link to={`/`} className="btn btn-dark text-white fs-6 me-2">Log out</Link> 
+                    </li>
+                </ul>
+            </div>
+            </li>
+        </ul>
+    </nav>
     <div className="container">
      <h1 className='text-danger'>Welcome!!</h1>
       <div className="form-container">
